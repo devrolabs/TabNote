24dabd3fa834eb368a3e37a1d224eaef4baffc33
Kesara Wimal <kesarawimal9@gmail.com>
2018-09-18 13:20:49 +0530

    done

--- popup.js
+++ popup.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let trigger = document.getElementById('trigger');
-    trigger.addEventListener('click', function() {
-        let info = [$('#url').val(), $('#count').val(), $("#async")[0].checked];
-        chrome.runtime.sendMessage({type: "trigger", message: info}, function (response) {});
+    let open = document.getElementById('open');
+    open.addEventListener('click', function() {
+        let family = $('#family').val();
+        let size = $('#size').val();
+        let color = $('#color').val();
+        let bg_color = $('#bg-color').val();
+        let bold = $('#bold').val();
+        let spell = $("#spell")[0].checked;
+        let url = 'data:text/html;charset=utf-8, <title> TabNote </title><body contenteditable style="font-family:'+ family +';font-weight:'+ bold +';background:'+ bg_color +';color:'+ color +';font-size:'+ size +';line-height:1.6;max-width:80rem;margin:0 auto;padding:2rem;" spellcheck="'+ spell +'">';
+        url = encodeURI(url);
+        chrome.windows.create(
+            {'url': url, 'type': 'popup', 'focused': true, height: 600, width: 1000},
+            function(chromeWindow) {
+
+            });
     });
 
     $('.simple-color-picker').colorpicker();
